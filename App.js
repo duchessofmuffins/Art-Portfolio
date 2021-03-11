574c1948c613620fc73c884ba1c69dc05f62a29f
Alice G <inkflight@gmail.com>
2021-03-10 21:49:20 -0600

    Commit issues resulting in manual uploads

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+
+import Home from './pages/Home.jsx';
+import Skills from './pages/Skills.jsx';
+import Contact from './pages/Contact.jsx';
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={Home}/>
+          <Route path="/skills" component={Skills}/>
+          <Route path="/contact" component={Contact}/>
+          <Route path="/projects" component={Projects}/>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# Art-Portfolio
-An interactive, experimental React site that showcases my best work in digital and traditional visual arts.
+# React Portfolio
+
+## Table of Contents
+
+* [Description](#description)
+* [Installation](#installation)
+* [Contributors](#contributors)
+* [Run](#run)
+* [Questions](#questions)
+
+## Description
+My basic React Portfolio
+
+## Installation
+npm install
+
+## Contributors
+Myself
+
+## Run
+npm start
+
+## Questions
+Questions?  Feel free to contact me at inkflight@gmail.com.
+You can find more of my work at [duchessofmuffins](https://github.com/undefined/).
